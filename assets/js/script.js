9dcaf07a185c8b946a41dcfffc95a1d31cb2fb69
Alexsandrrh <sadovspbsasha@outlook.com>
2018-02-28 11:27:45 +0300

    update mobile-nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $('.nav-toggle').click(function () {
         $('.mobile-nav').toggleClass('-open');
     })
+    $('.nav-menu a').click(function () {
+        $('.mobile-nav').removeClass('-open');
+        $('.mobile-nav').addClass('-close');
+        $('.mobile-nav').removeClass('-close');
+    })
 });
---
+++ assets/js/scroll.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.scroll').click(function() {
+        var anchor = $(this).attr("href");
+        jQuery("html,body").stop().animate({
+            scrollTop: $(anchor).offset().top
+        }, 1000);
+        return false;
+    });
+});
